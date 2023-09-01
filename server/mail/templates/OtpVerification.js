08a1293edaf91e6a2a76e41ab8e2648ca0484354
Aman Verma <aman@Amans-MacBook-Air.local>
2023-09-01 09:37:28 +0530

    adding site name in email templates

--- server/mail/templates/OtpVerification.js
+++ server/mail/templates/OtpVerification.js
@@ -8,17 +8,17 @@ const otpTemplate = (otp) => {
   <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
   <div style="margin:50px auto;width:80%;padding:20px 0">
   <div style="border-bottom:5px solid #eee">
-    <a href="" style="font-size:30px;color: #f7c800;text-decoration:none;font-weight:600">CodeCrafter</a>
+    <a href="" style="font-size:30px;color: #f7c800;text-decoration:none;font-weight:600">SocialFlock</a>
   </div>
   <p style="font-size:15px">Hello Folk</p>
-  <p>Thank you for registering with CodeCrafter. To complete your registration, please use the following OTP
+  <p>Thank you for registering with SocialFlock. To complete your registration, please use the following OTP
   (One-Time Password) to verify your account</p>
   <p>Remember, Never share this OTP with anyone.</p>
   <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${otp}</h2>
-  <p style="font-size:15px;">Regards,<br />Team CodeCrafter</p>
+  <p style="font-size:15px;">Regards,<br />Team SocialFlock</p>
   <hr style="border:none;border-top:5px solid #eee" />
   <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
-    <p>CodeCrafter</p>
+    <p>SocialFlock</p>
     <p>address</p>
    </div>
   </div>
--- server/mail/templates/ResetPassword.js
+++ server/mail/templates/ResetPassword.js
@@ -8,20 +8,20 @@ const resetPasswordTemplate = (link) => {
     <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
     <div style="margin:50px auto;width:80%;padding:20px 0">
     <div style="border-bottom:5px solid #eee">
-      <a href="" style="font-size:30px;color: #f7c800;text-decoration:none;font-weight:600">CodeCrafter</a>
+      <a href="" style="font-size:30px;color: #f7c800;text-decoration:none;font-weight:600">SocialFlock</a>
     </div>
    
-    <p style="font-size:15px">We have received a request to reset your password for your CodeCrafter account. To complete the password reset process, please follow the link below:
+    <p style="font-size:15px">We have received a request to reset your password for your SocialFlock account. To complete the password reset process, please follow the link below:
    </p>
 
     <p style="font-size:15px">Please note that this link is valid for the next 30 minutes. If you did not initiate this request or no longer wish to reset your password, please ignore this email.</p>
     <p>
     ${link}
     </p>
-    <p style="font-size:15px;">Regards,<br />Team CodeCrafter</p>
+    <p style="font-size:15px;">Regards,<br />Team SocialFlock</p>
     <hr style="border:none;border-top:5px solid #eee" />
     <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
-      <p>CodeCrafter</p>
+      <p>SocialFlock</p>
       <p>address</p>
      </div>
     </div>
